dc640b035cabc24aac7bf9f2e7d130737df49ad3
Maurizio-F <end2zone@googlemail.com>
2021-05-04 15:23:38 +0200

    Add Layout

--- main.js
+++ main.js
@@ -1,12 +1,38 @@
 import "./style.css";
 import { createElement } from "./utils/elements";
 
-const mainElement = createElement("main", {
-  className: "main",
+const header = createElement("header", {
+  className: "header",
   children: [
-    createElement("input", { placeholder: "Enter name" }),
-    createElement("section", { innerText: "Results" }),
+    createElement("h1", {
+      className: "header_title",
+      innerText: "Rick and Morty",
+    }),
+    createElement("input", {
+      className: "input",
+      placeholder: "Search character",
+      autofocus: true,
+    }),
   ],
 });
 
-document.querySelector("#app").append(mainElement);
+const section = createElement("section", {
+  className: "results",
+  children: [
+    createElement("div", {
+      innerText: "No results",
+    }),
+  ],
+});
+
+const footer = createElement("footer", {
+  className: "footer",
+  children: [
+    createElement("div", {
+      className: "footer-element",
+      children: [createElement("span", { innerText: "This is the footer" })],
+    }),
+  ],
+});
+
+document.querySelector("#app").append(header, section, footer);
