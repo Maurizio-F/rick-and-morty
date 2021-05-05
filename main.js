fdf45782d68270bc90642c28d27ff987187b3204
Maurizio-F <end2zone@googlemail.com>
2021-05-05 15:15:11 +0200

    ✨ Add search filter

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 import { createCharacterElement } from "./components/character";
 import "./style.css";
-import { createElement } from "./utils/elements";
+import { createElement, removeAllChildren } from "./utils/elements";
 import { getCharacters } from "./utils/api";
-
-getCharacters().then((characters) => {
-  const characterElements = characters.map(createCharacterElement);
-  characterSection.append(...characterElements);
-});
 
 const characterSection = createElement("section", {
   className: "results",
@@ -25,14 +20,18 @@ const header = createElement("header", {
       className: "input",
       placeholder: "Search character",
       autofocus: true,
+      oninput: (event) => {
+        removeAllChildren(characterSection);
+
+        const search = event.target.value;
+        getCharacters(search).then((characters) => {
+          const characterElements = characters.map(createCharacterElement);
+          characterSection.append(...characterElements);
+        });
+      },
     }),
   ],
 });
-
-// const section = createElement("section", {
-//   className: "results",
-// children: characters.map(createCharacterElement),
-// });
 
 const footer = createElement("footer", {
   className: "footer",
